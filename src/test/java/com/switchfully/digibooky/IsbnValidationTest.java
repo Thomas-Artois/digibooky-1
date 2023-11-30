779d09637cc203510f62263a729f83424b27b733
Jussi Ccoyllo <jussi.ccoyllo@smals.be>
2023-11-30 15:57:55 +0100

    add isbnValidation + test isbnValidation

---
+++ src/test/java/com/switchfully/digibooky/IsbnValidationTest.java
@@ -0,0 +1,34 @@
+package com.switchfully.digibooky;
+
+import com.switchfully.digibooky.domain.IsbnValidation;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IsbnValidationTest {
+    @Test
+    void givenValidIsbn_ThenReturnTrue(){
+        //given
+        String isbnNumber = "9780596528126";
+
+        //when
+        boolean actual = IsbnValidation.isIsbn13(isbnNumber);
+
+        //then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void givenValidIsbn_ThenReturnFalse(){
+        //given
+        String isbnNumber = "8780596528126";
+
+        //when
+        boolean actual = IsbnValidation.isIsbn13(isbnNumber);
+
+        //then
+        assertThat(actual).isFalse();
+    }
+}
