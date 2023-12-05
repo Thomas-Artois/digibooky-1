8ba248b7eb2ff85ea545501401a1992dc60a241e
Karel Van Geerdeghom <karel.van.geerdeghom@gmail.com>
2023-12-05 10:27:45 +0100

    Make userMapper in UserMapperTest final

--- src/test/java/com/switchfully/digibooky/mapper/UserMapperTest.java
+++ src/test/java/com/switchfully/digibooky/mapper/UserMapperTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserMapperTest {
-    private UserMapper userMapper = new UserMapper();
+    private final UserMapper userMapper = new UserMapper();
 
     @Test
     void givenUser_whenMapUserToUserDto_thenGetUserDto() {
